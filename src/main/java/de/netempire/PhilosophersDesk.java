85a5faf03ef238b26c0d5f1244c371ba1ec52c9a
m_goebel <murat.goebel@googlemail.com>
2020-04-14 17:16:34 +0200

    17:15 140420

--- src/main/java/de/netempire/PhilosophersDesk.java
+++ src/main/java/de/netempire/PhilosophersDesk.java
@@ -31,15 +31,15 @@ public class PhilosophersDesk {
     static Thread herderThread = new Thread(herder);
     static Date start = Calendar.getInstance().getTime();
     static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    static Thread task2Thread = new Thread();
+    static Runnable controller;
 
     public static void main(String[] args) {
         PhilosophersDesk.startProcess();
     }
 
     private static void startProcess() {
-        start();
         initialize();
+        start();
     }
 
     public static void initialize(){
@@ -48,8 +48,8 @@ public class PhilosophersDesk {
         herder.setEatingTime(300);
         platon.setEatingTime(1500);
         platon.setEatingTime(500);
-        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-        Runnable controller = () -> {
+
+        controller = () -> {
             if(!platonThread.isAlive() && !herderThread.isAlive() && !aristotelesThread.isAlive() && !fichteThread.isAlive() && !schlegelThread.isAlive()){
                 platon.stop();
                 herder.stop();
@@ -61,8 +61,6 @@ public class PhilosophersDesk {
                 ResultLogger.log("Die Philosophen haben " + computeDuration(start, Calendar.getInstance().getTime()) + " Sekunden zusammen am Tisch gesessen.");
             }
         };
-        executor.scheduleAtFixedRate(controller, 0, 4, TimeUnit.SECONDS);
-        task2Thread = new Thread(controller);
     }
 
     public static void start(){
@@ -71,7 +69,7 @@ public class PhilosophersDesk {
         schlegelThread.start();
         fichteThread.start();
         herderThread.start();
-        task2Thread.start();
+        executor.scheduleAtFixedRate(controller, 0, 4, TimeUnit.SECONDS);
     }
 
     public static int computeDuration(Date to, Date from) {
